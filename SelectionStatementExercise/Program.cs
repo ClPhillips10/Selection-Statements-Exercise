a0c4963f9ef71d762ed2558eb7b3928250baa318
ClPhillips10 <Lakephillips3232@gmail.com>
2023-05-09 09:44:52 -0500

    Exercise 1 fixed

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -5,21 +5,19 @@
         static void Main(string[] args)
         {
             //Fav number generator
-            //var r = new Random();
-            //var favNumber = r.Next(1, 1000);
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
 
+
+            /* test value
             int favNumber = 9;
+            */
 
             //Whats on screen and collect user var
             Console.Write("Guess my favorite number ");
             var userInput = int.Parse(Console.ReadLine());
 
-           
-
-
-
-
-            //If value is too low
+            //If value is too low------------------------------
 
             if (userInput < favNumber)
             {
@@ -27,14 +25,14 @@
                 Console.WriteLine("You're value is too low ");
             }
 
-            //if value is too high
+            //if value is too high------------------------------
             else if (userInput > favNumber)
             {
                 Console.WriteLine("Value Was too High");
 
             }
 
-            // user wins
+            // user wins----------------------------
             else
             {
                 Console.WriteLine("Congrats thats correct!");
